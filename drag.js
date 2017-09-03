c76bfec29fbc7aa710d8bdb7dedc123e5d31e036
sophieba <lxhsophie@gmail.com>
2017-09-03 21:04:23 +0800

    change drag.js

--- drag.js
+++ drag.js
@@ -2,9 +2,10 @@ function drag(id){
   var obj=document.getElementByI("id");
   disX=0;
   disY=0;
-  obj.onmousedown=function(ev){
-   disX =ev.pageX -obj.offsetLeft;
-   disY =ev.pageY -obj.offsetLeft;
-   
+  obj.onmousedown=function(){
+      
   }
+obj.onmousemove=function(){
+}
+
 }
