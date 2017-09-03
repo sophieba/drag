3c4360760df2b671b5b1a41148fb6488648a907b
sophieba <lxhsophie@gmail.com>
2017-09-03 14:26:01 +0800

    change drag.js

--- drag.js
+++ drag.js
@@ -2,7 +2,9 @@ function drag(id){
   var obj=document.getElementByI("id");
   disX=0;
   disY=0;
-  obj.onmousedown=function(){
-      
+  obj.onmousedown=function(ev){
+   disX =ev.pageX -obj.offsetLeft;
+   disY =ev.pageY -obj.offsetLeft;
+   
   }
 }
